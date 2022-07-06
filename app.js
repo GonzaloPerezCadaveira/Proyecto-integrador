9d2b8bca2b5bf337c65f6fa15ffae638a3bd4f61
GonzaloPerezCadaveira <gperezmdp@gmail.com>
2022-07-06 01:14:40 -0300

    link href

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ const express = require ('express')
 const app = express ()
 const path = require ('path')
 
-app.use(express.static('Public'));
+app.use(express.static('public'));
 
 app.listen (3050, () => { 
 console.log ('servidor corriendo')
 })
 
 app.get('/', (req, res) => {
+    res.sendFile (path.resolve(__dirname,'./views/index.html'))
+      });
+app.get('/Carrito', (req, res) => {
+    res.sendFile (path.resolve(__dirname,'./views/Carrito.html'))
+      });
+app.get('/login', (req, res) => {
     res.sendFile (path.resolve(__dirname,'./views/login.html'))
       });
