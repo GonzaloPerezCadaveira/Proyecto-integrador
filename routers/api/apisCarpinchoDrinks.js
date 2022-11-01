7a39075df8244c81e26aa9a4bc097add2deff7d4
AgustinXAlv1 <alvarezagustinx@gmail.com>
2022-11-01 16:57:38 -0300

    Correccion de rutas para APIs

--- routers/api/apisCarpinchoDrinks.js
+++ routers/api/apisCarpinchoDrinks.js
@@ -6,7 +6,13 @@ const router = express.Router();
 
 const userController = require("../../src/controllers/api/controllerUsersApi");
 
+const productController = require("../../src/controllers/api/controllerProductApi");
+
 // Middlewares
+
+router.get('/products',productController.list)
+router.get('/product/:id',productController.detail)
+
 
 router.get('/users',userController.list)
 router.get('/user/:id',userController.detail)
